356b3c4667cdb4bbacfc36e77c5c7d2640a3fe88
rngustj9139 <mnb9139@khu.ac.kr>
2022-07-23 06:11:09 +0900

    environment configuration

---
+++ src/main/java/Koo/personalsecurenotewebservice/PersonalSecureNoteWebServiceApplication.java
@@ -0,0 +1,13 @@
+package Koo.personalsecurenotewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalSecureNoteWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalSecureNoteWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Koo/personalsecurenotewebservice/PersonalSecureNoteWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Koo.personalsecurenotewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalSecureNoteWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
