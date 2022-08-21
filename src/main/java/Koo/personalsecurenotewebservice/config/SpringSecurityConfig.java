328863a24212547cc6583033305ed218858b6faf
rngustj9139 <mnb9139@khu.ac.kr>
2022-08-21 19:38:51 +0900

    Using jwt instead of session

--- src/main/java/Koo/personalsecurenotewebservice/config/SpringSecurityConfig.java
+++ src/main/java/Koo/personalsecurenotewebservice/config/SpringSecurityConfig.java
@@ -1,7 +1,10 @@
 package Koo.personalsecurenotewebservice.config;
 
 import Koo.personalsecurenotewebservice.customfilter.StopwatchFilter;
+import Koo.personalsecurenotewebservice.jwt.JwtAuthenticationFilter;
+import Koo.personalsecurenotewebservice.jwt.JwtAuthorizationFilter;
 import Koo.personalsecurenotewebservice.user.User;
+import Koo.personalsecurenotewebservice.user.UserRepository;
 import Koo.personalsecurenotewebservice.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -12,8 +15,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -26,6 +32,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception { // 시큐리티의 상세 설정 정의
@@ -34,12 +41,29 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 new StopwatchFilter(),
                 WebAsyncManagerIntegrationFilter.class
         );
+
         // basic authentication filter
         http.httpBasic().disable(); // basic authentication filter 비활성화 (httpBasic() : Http basic Auth 기반으로 로그인 인증창이 뜸. 기본 인증 로그인을 이용하지 않으면 disable )
+
         // csrf
         http.csrf(); // 사이트 간 요청 위조(Cross-site Request Forgery) 방지 ex) 이러한 공격을 하기 위하여 해커는 우선 공격을 할 사이트를 먼저 분석합니다. 예를 들어, 나무위키의 경우에 토론은 namu.wiki/topic/ 이라고 시작하며 뒤에 숫자가 붙는 형식인데 이 뒤의 숫자에 패턴이 있습니다.(실제론 토론이 개설된 순서대로 붙는 일련번호이다.) 그러면 이 패턴을 이용하여 일반적인 방법으로 접근할 수 없는 페이지를 오픈 한다든지, 개발에 사용되고 실제로 사용하지 않는 샘플 페이지를 찾아낸다든지 이러한 방법이 가능합니다.
+
         // remember-me
         http.rememberMe(); // 사용자 세션이 만료되고 웹 브라우저가 종료된 후에도 애플리케이션이 사용자의 정보를 기억한다. (로그인 유지하기)
+
+        // session 대신 토큰 이용
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 우리는 JWT를 사용하므로(토큰방식) 세션이 필요없다 따라서 Stateless로 설정해둔다.
+
+        // jwt filter 등록
+        http.addFilterBefore(
+                new JwtAuthenticationFilter(authenticationManager()),
+                UsernamePasswordAuthenticationFilter.class // JwtAuthenticationFilter 필터는 UsernamePasswordAuthenticationFilter 필터의 앞쪽에 위치
+        ).addFilterBefore(
+                new JwtAuthorizationFilter(userRepository),
+                BasicAuthenticationFilter.class
+        );
+
         // authorization
         http.authorizeRequests() // 인가 설정 (경로별로 권한을 설정)
                 // /와 /home /signup은 모두에게 허용
