ba3f07364147e5b5389d16b531ee5cd02c8f1cca
rngustj9139 <mnb9139@khu.ac.kr>
2022-08-21 19:22:01 +0900

    Add comment in (JwtAuthorizationFilter)

--- src/main/java/Koo/personalsecurenotewebservice/jwt/JwtAuthorizationFilter.java
+++ src/main/java/Koo/personalsecurenotewebservice/jwt/JwtAuthorizationFilter.java
@@ -35,10 +35,10 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter { // OncePerReq
 
         try {
             // cookie 에서 JWT token을 가져옵니다.
-            token = Arrays.stream(request.getCookies())
+            token = Arrays.stream(request.getCookies()) // 여러개의 쿠기가 들어오므로 스트림을 건다.
                     .filter(cookie -> cookie.getName().equals(JwtProperties.COOKIE_NAME)).findFirst() // findFirst는 filter로 걸러진 쿠키중 첫번째 쿠키를 찾게함
                     .map(Cookie::getValue) // .map(cookie->cookie.getValue())와 같다, cookie.getName()은 저장된 데이터 값에 대한 이름을 가져오는 메소드이고cookie.getValue()는 해당 이름에 저장된 데이터 값을 가져온다.
-                    .orElse(null);
+                    .orElse(null); // getValue를 통해 값을 찾을려하지만 없다면 null을 반환한다 => token 변수에 null이 저장되게 된다.
         } catch (Exception ignored) {
         }
 
